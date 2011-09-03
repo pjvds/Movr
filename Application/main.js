d4af0901926ab45b8c736d54a9b0a80e2367ebd0
pjvds <pj@born2code.net>
2011-09-03 13:00:55 +0000

    Configured socket.io.

--- Application/main.js
+++ Application/main.js
@@ -1,8 +1,13 @@
 var http = require('http');
 var socket = require('socket.io');
 
-http.createServer(function (req, res) {
+var app = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(process.env.C9_PORT, "0.0.0.0");
-console.log('Server running at http://movr.pjvds.c9.io');
+});
+
+socket.listen(app);
+
+app.listen(process.env.C9_PORT, "0.0.0.0");
+
+console.log('Server running at http://movr.pjvds.c9.io:'+process.env.C9_PORT);
