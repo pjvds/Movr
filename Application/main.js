cc5c7810523fda1e6b2db48dd6bc1d98977f792f
pjvds <pj@born2code.net>
2011-09-03 12:46:19 +0000

    Configured hello world NodeJS.

---
+++ Application/main.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(process.env.C9_PORT, "0.0.0.0");
+console.log('Server running at http://movr.pjvds.c9.io');
